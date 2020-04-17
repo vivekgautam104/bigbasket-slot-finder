81d26097b06604002c6b4d694c9926900f70fac3
Gautam, Vivek <VGautam@Scholastic.com>
2020-04-17 03:01:47 -0400

    Executables and documentation updated

--- README.md
+++ README.md
@@ -4,24 +4,33 @@ A cross platform tool that finds available delivery slots for Big Basket Home de
 ## Pre-requisites: 
 
 1. Please make sure that you have Google Chrome browser installed on your system. The tools works only with Google Chrome.
-2. Fill up you cart/basket with you complete order before running the process.
+2. Fill up your cart/basket with you complete order before running the process.
 3. Turn up the volumne of your system for the voice notification.
 
 ## Instructions:
 
+## Option 1 - Use of executables
+
 1. Download the respective executable file for MAC OS and Windows OS:
-	* MAC - SlotFinder/mac/dist/bigbasket_slotfinder
-	* Windows - SlotFinder/win/dist/bigbasket_slotfinder.exe
+	* MAC - Click here [a link] (https://drive.google.com/uc?export=download&id=1AOPl3jIzNtWpxP543o2XUqb8EsHQe7Wv)
+	* Windows - Click here [a link] (https://drive.google.com/uc?export=download&id=1Tn1eu60OteRH0JkfF0HqnmkSU9UEwSlj)
 2. Run the respective executable file.
 3. The tool will automatically download the compatible Google Chrome driver during the first run in line with you browser version. If promted, click allow access.
 4. A new incognito Google Chrome window will be launched. Login to your Big Basket account and select your delivery location within 60 seconds:
-![BigBasket Login](/images/login.png)
+![BigBasket Login](https://raw.githubusercontent.com/vivekgautam104/bigbasket-slot-finder/master/SlotFinder/images/login.png)
 5. After 60 seconds, the launched Google Chrome window will be minimized and process will keep running the background. You can always open the Google Chrome window and check the status. DO NOT explicitly close the launched window if you want to keep the process running in the background. 
 6. Your Big Basket cart/basket with complete order at checkout page would look like this:
-![BigBasket Login](/images/mybasket.png)
+![BigBasket Cart](https://raw.githubusercontent.com/vivekgautam104/bigbasket-slot-finder/master/SlotFinder/images/mybasket.png)
 7. You will be alerted once a slot is found. 
 8. The voice notification will be sent out when a slot is found.
 9. Once you're notified, quickly select a slot and finish checking out because available slots disappear almost instantly.
+
+## Option 2 - Execution of Source Code 
+
+1. Make sure Python 3.x is installed on your system
+2. Clone the repo
+3. Run `<pip3 install -r requirements.txt>`
+4. Run `<python3 bigbasket_slotfinder.py>`
 
 ## Inspiration and idea behind this tool:
 
--- SlotFinder/mac/dist/bigbasket_slotfinder.app/Contents/Resources/__boot__.py
+++ SlotFinder/mac/dist/bigbasket_slotfinder.app/Contents/Resources/__boot__.py
@@ -11,49 +11,6 @@ def _reset_sys_path():
 _reset_sys_path()
 
 
-def _site_packages(prefix, real_prefix, global_site_packages):
-    import site
-    import sys
-    import os
-
-    paths = []
-
-    paths.append(
-        os.path.join(prefix, "lib", "python" + sys.version[:3], "site-packages")
-    )
-    if os.path.join(".framework", "") in os.path.join(prefix, ""):
-        home = os.environ.get("HOME")
-        if home:
-            paths.append(
-                os.path.join(
-                    home, "Library", "Python", sys.version[:3], "site-packages"
-                )
-            )
-
-    # Work around for a misfeature in setuptools: easy_install.pth places
-    # site-packages way to early on sys.path and that breaks py2app bundles.
-    # NOTE: this is hacks into an undocumented feature of setuptools and
-    # might stop to work without warning.
-    sys.__egginsert = len(sys.path)
-
-    for path in paths:
-        site.addsitedir(path)
-
-    # Ensure that the global site packages get placed on sys.path after
-    # the site packages from the virtual environment (this functionality
-    # is also in virtualenv)
-    sys.__egginsert = len(sys.path)
-
-    if global_site_packages:
-        site.addsitedir(
-            os.path.join(
-                real_prefix, "lib", "python" + sys.version[:3], "site-packages"
-            )
-        )
-
-
-_site_packages('/Users/vivekgau/Documents/Vivek/Code-Scholastic/SlotFinder/venv', '/Library/Frameworks/Python.framework/Versions', 0)
-
 def _chdir_resource():
     import os
 
@@ -63,25 +20,17 @@ def _chdir_resource():
 _chdir_resource()
 
 
-def _setup_ctypes():
-    from ctypes.macholib import dyld
-    import os
+def _disable_linecache():
+    import linecache
 
-    frameworks = os.path.join(os.environ["RESOURCEPATH"], "..", "Frameworks")
-    dyld.DEFAULT_FRAMEWORK_FALLBACK.insert(0, frameworks)
-    dyld.DEFAULT_LIBRARY_FALLBACK.insert(0, frameworks)
+    def fake_getline(*args, **kwargs):
+        return ""
 
-
-_setup_ctypes()
+    linecache.orig_getline = linecache.getline
+    linecache.getline = fake_getline
 
 
-def _path_inject(paths):
-    import sys
-
-    sys.path[:0] = paths
-
-
-_path_inject(['/Users/vivekgau/Documents/Vivek/Code-Scholastic/SlotFinder'])
+_disable_linecache()
 
 
 import re
@@ -111,32 +60,51 @@ def _run():
     import site  # noqa: F401
 
     sys.frozen = "macosx_app"
+    base = os.environ["RESOURCEPATH"]
 
     argv0 = os.path.basename(os.environ["ARGVZERO"])
     script = SCRIPT_MAP.get(argv0, DEFAULT_SCRIPT)  # noqa: F821
 
-    sys.argv[0] = __file__ = script
+    path = os.path.join(base, script)
+    sys.argv[0] = __file__ = path
     if sys.version_info[0] == 2:
-        with open(script, "rU") as fp:
+        with open(path, "rU") as fp:
             source = fp.read() + "\n"
     else:
-        with open(script, "rb") as fp:
+        with open(path, "rb") as fp:
             encoding = guess_encoding(fp)
 
-        with open(script, "r", encoding=encoding) as fp:
+        with open(path, "r", encoding=encoding) as fp:
             source = fp.read() + "\n"
 
         BOM = b"\xef\xbb\xbf".decode("utf-8")
-
         if source.startswith(BOM):
             source = source[1:]
 
-    exec(compile(source, script, "exec"), globals(), globals())
+    exec(compile(source, path, "exec"), globals(), globals())
 
 
-DEFAULT_SCRIPT='/Users/vivekgau/Documents/Vivek/Code-Scholastic/SlotFinder/bigbasket_slotfinder.py'
+def _setup_ctypes():
+    from ctypes.macholib import dyld
+    import os
+
+    frameworks = os.path.join(os.environ["RESOURCEPATH"], "..", "Frameworks")
+    dyld.DEFAULT_FRAMEWORK_FALLBACK.insert(0, frameworks)
+    dyld.DEFAULT_LIBRARY_FALLBACK.insert(0, frameworks)
+
+
+_setup_ctypes()
+
+
+def _boot_tkinter():
+    import os
+
+    resourcepath = os.environ["RESOURCEPATH"]
+    os.putenv("TCL_LIBRARY", os.path.join(resourcepath, "lib/tcl8"))
+    os.putenv("TK_LIBRARY", os.path.join(resourcepath, "lib/tk8.6"))
+_boot_tkinter()
+
+
+DEFAULT_SCRIPT='bigbasket_slotfinder.py'
 SCRIPT_MAP={}
-try:
-    _run()
-except KeyboardInterrupt:
-    pass
+_run()
